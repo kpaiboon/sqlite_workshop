997fc86f2e621f802616c1f356d465efb9d38b22
paiboon.kupthanakorn <kpaiboon@gmail.com>
2022-01-04 14:09:49 +0700

    Id BigInt auto_increment

--- mysqldev/db_createtable_insert_query.py
+++ mysqldev/db_createtable_insert_query.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine,DateTime,func,TIMESTAMP, Column, Integer, Float, String, or_, update
+from sqlalchemy import create_engine,DateTime,func,TIMESTAMP, Column,BigInteger, Integer, Float, String, or_, update
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 
 #engine = create_engine('sqlite:///cmstock.db')
 #@ MariaDB: Create DB "cmdev" charset=utf8mb4_general_ci (faster sorting), for use sqlalchemy charset = utf8mb4 
@@ -13,11 +12,11 @@ Base = declarative_base()
 
 class Product(Base):
     __tablename__ ='product'
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
     name = Column(String(50))
     price = Column(Float)
     stock = Column(Integer)
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    #time_created = Column(DateTime(timezone=True), server_default=func.now())
     #time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     time_updated = Column(DateTime(timezone=True), nullable=False,server_default=func.now(),onupdate=func.now())
     #ts= Column('time_updated', TIMESTAMP, nullable=False,server_default=func.now(),onupdate=func.now())
