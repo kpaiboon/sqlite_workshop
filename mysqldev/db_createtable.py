8efdcbd953bb7b2127119a5eda7084d46929aebf
paiboon.kupthanakorn <kpaiboon@gmail.com>
2022-01-04 10:40:47 +0700

    from sqlite

--- mysqldev/db_createtable.py
+++ mysqldev/db_createtable.py
@@ -1,1 +1,28 @@
-.
+from sqlalchemy import create_engine,DateTime,func,TIMESTAMP, Column, Integer, Float, String, or_, update
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:///cmstock.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ ='product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    price = Column(Float)
+    stock = Column(Integer)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    #time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    time_updated = Column(DateTime(timezone=True), nullable=False,server_default=func.now(),onupdate=func.now())
+    #ts= Column('time_updated', TIMESTAMP, nullable=False,server_default=func.now(),onupdate=func.now())
+    
+Base.metadata.create_all(engine) # create table and file*.db
+
+# Prepare data; Insert(add), commit(go)
+productone = Product(name='Arduino', price=100.50, stock=10)
+session.add(productone)
+session.commit()
